cd0b48b9da908a5a0bf84f581ecbfad3fe17886e
mtumlin <mattias.tumlin@gmail.com>
2015-12-23 12:46:37 +0100

    updated settings to work with heroku

--- blogsite/settings/production.py
+++ blogsite/settings/production.py
@@ -1,7 +1,23 @@
 from .base import *
+import dj_database_url
+from django.core.exceptions import ImproperlyConfigured
 
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
 
 DEBUG = False
+TEMPLATE_DEBUG = False
+SECRET_KEY = get_env_variable('SECRET_KEY')
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
 
 try:
     from .local import *
